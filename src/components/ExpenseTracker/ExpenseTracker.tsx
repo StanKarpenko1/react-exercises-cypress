19fa08ab6c3db51340de775a8964a1a4ae9ea930
staskarpenko1 <karpenko.stas@gmail.com>
2025-04-07 00:28:34 -0400

    expense-tracker added

--- src/components/ExpenseTracker/ExpenseTracker.tsx
+++
@@ -1,8 +0,0 @@
-
-const ExpenseTracker = () => {
-  return (
-    <div>ExpenseTracker</div>
-  )
-}
-
-export default ExpenseTracker
--- src/pages/ExpenseTrackerPage.tsx
+++ src/pages/ExpenseTrackerPage.tsx
@@ -1,13 +1,28 @@
-import ExpenseTracker from "../components/ExpenseTracker/ExpenseTracker"
-import GoHomeButton from "../components/GoHome/GoHome"
+import { useState } from "react";
+import ExpenceList from "../components/expence-tracker/ExpenseList";
+import GoHomeButton from "../components/GoHome/GoHome";
 
 const ExpenseTrackerPage = () => {
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Prod1", amount: 10, category: "Utilities" },
+    { id: 2, description: "Prod2", amount: 10, category: "Utilities" },
+    { id: 3, description: "Prod3", amount: 10, category: "Other" },
+    { id: 4, description: "Prod4", amount: 10, category: "Utilities" },
+  ])
+
+  const handleDelete = (id: number) => setExpenses(expenses.filter(e => e.id !== id))
+
+
   return (
     <div>
-        <ExpenseTracker />
-        <GoHomeButton />
+      <ExpenceList 
+        expenses={expenses} 
+        onDelete={ handleDelete } />
+ 
+      <GoHomeButton />
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseTrackerPage
+export default ExpenseTrackerPage;
