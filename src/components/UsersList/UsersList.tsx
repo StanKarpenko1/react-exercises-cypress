071b22d97ebc9607c7931e86bda79a9e5615798c
staskarpenko1 <karpenko.stas@gmail.com>
2025-04-22 23:53:14 -0400

    user methods

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,46 +1,108 @@
-import axios, { CanceledError } from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import  {CanceledError} from "../../services/api-client";
+import userService, { IUser } from "../../services/user-service";
 
-interface IUser {
-    id: number,
-    name: string
-}
+
+
 
 const UsersList = () => {
-    const [users, setUsers] = useState<IUser[]>([]);
-    const [error, setError] = useState('');
-    const [isLoading, setLoading] = useState(true)
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        const controller = new AbortController();
+    const { request, cancel } = userService.getAllUsers();
 
-        axios   
-            .get<IUser[]>('https://jsonplaceholder.typicode.com/users', { signal: controller.signal })
-            .then((res) => {
-                setUsers(res.data)
-                setLoading(false)
-            })
-            .catch((err) =>  {
-                if (err instanceof CanceledError) return
-                setError(err.message)
-                setLoading(false)
-            });
+      request.then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
 
+    return () => cancel();
+  }, []);
 
-        return () => controller.abort();
-    }, []);
+  const deleteUser = (user: IUser) => {
+    const originalUsers = [...users];
+    
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    userService.deleteUser(user.id)
+        .catch((err) => {
+            setError(err.message);
+            setUsers(originalUsers);     
+        });
+  };
+
+  const addUser = () => {
+    const newUser = { id: 0, name: "Stan" };
+    const originalUsers = [...users];
+
+    setUsers([newUser, ...users]);
+
+   userService.addUser(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = ( user : IUser ) => {
+
+    const updatedUser = { ...user, name: (user.name).toUpperCase()  }
+    
+    const originalUsers = [...users];
+
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+
+    userService.updateUser(updatedUser)
+        .catch((err) => {
+            setError(err.message);
+            setUsers(originalUsers);
+          }); 
+  }
 
   return (
     <>
-        <p> Users List Page</p>
-        {isLoading && <div className="spinner-border"></div>}
-        {error && <p className="text-danger">{error}</p>}
-        <ul>
-            {users.map(user => <li key={user.id}> { user.name} </li>)}
-        </ul>
-    </>
-  )
-}
+      <p> Users List Page</p>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
 
-export default UsersList
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div>
+              <button 
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+               
+               >Update</button>
+
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersList;
---
+++ src/services/user-service.ts
@@ -0,0 +1,26 @@
+import apiClient from "./api-client";
+
+export interface IUser {
+    id: number;
+    name: string;
+  }
+
+class UserService {
+    getAllUsers() {
+        const controller = new AbortController();
+
+        const request =  apiClient.get<IUser[]>('/users', { signal: controller.signal });
+        return { request , cancel: () => {controller.abort()} }
+    }
+    deleteUser(id: number) {
+        return apiClient.delete(`/users/${id}`)
+    }
+    addUser (user: IUser) {
+       return  apiClient.post('/users', user)
+    }
+    updateUser (user: IUser) {
+        return apiClient.patch(`users/${user.id}`, user)
+    }
+}
+
+export default new  UserService();
